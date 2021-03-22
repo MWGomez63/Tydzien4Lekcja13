4b8d21696dd3d757b2bb2e88e65d7b39172c1ecc
mwgomez <mwybranczyk@op.pl>
2021-03-22 21:43:10 +0100

    Nowa lekcja

---
+++ Lekcja13/FizzBuzz.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lekcja13
+{
+    public class FizzBuzz
+    {
+        private int CustomerNumber;
+
+        public FizzBuzz(int number)
+        {
+            CustomerNumber = number;
+        }
+
+        public string GetAnswer()
+        {
+            if ((CustomerNumber % 3 == 0) && (CustomerNumber % 5 == 0))
+            {
+                return "FizzBuzz";
+            }
+            else
+            {
+                if (CustomerNumber % 3 == 0)
+                {
+                    return "Fizz";
+                }
+                else
+                {
+                    if (CustomerNumber % 5 == 0)
+                    {
+                        return "Buzz";
+                    }
+                    else
+                    {
+                        return CustomerNumber.ToString();
+                    }
+                }
+            }
+        }
+    }
+}
+
---
+++ Lekcja13/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Lekcja13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("K-koniec pracy");
+            Console.WriteLine();
+
+            while (true)
+            {
+                int number = GetValue();
+
+                FizzBuzz game = new FizzBuzz(number);
+                string answer = game.GetAnswer();
+                Console.WriteLine(answer);
+            }
+        }
+
+        private static int GetValue()
+        {
+            while (true)
+            {
+                Console.Write("Podaj liczbę: ");
+                var val = Console.ReadLine();
+
+                if (val.ToUpper() == "K")
+                {
+                    Console.WriteLine("Wybrano zakónczenie pracy");
+                    Environment.Exit(0);
+                }
+
+                if (!int.TryParse(val, out int number))
+                {
+                    Console.WriteLine("Podałeś nieprawidłowe dane. Spróbuj ponownie.");
+                    continue;
+                }
+
+                return number;
+            }
+        }
+    }
+}
